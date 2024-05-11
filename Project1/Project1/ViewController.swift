757f6e4d5d9fa48bb402439def1bbaf0a59f4bca
Mikhail <33933295+zhuzhman@users.noreply.github.com>
2024-05-11 23:07:15 +0300

    video 1 complete

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mikhail Zhuzhman on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
